31a11a684494b94ce062531e1d8c548d73ade4e5
Pontus Pohl <pontus.pohl@endian.se>
2016-02-25 14:19:05 +0100

    Wrapped factory in its own class and added msg check

--- src/twistedSocketServer.py
+++ src/twistedSocketServer.py
@@ -1,27 +1,48 @@
 from twisted.internet.protocol import Factory, Protocol
 from twisted.internet import reactor
 import sys
+import serial
 
-class casperProtocol(Protocol):
+class CasperProtocol(Protocol):
+    def __init__(self,clients):
+        print 'new protocol instance'
+        self.clients = clients
+
     def connectionMade(self):
-        self.factory.clients.append(self)
+        self.clients.append(self)
         print 'A client has connected'
-        print "clients are ", self.factory.clients
+        print "clients are ", self.clients
     def connectionLost(self, reason):
-        self.factory.clients.remove(self)
+        self.clients.remove(self)
         print 'A client disconnected'
-        print "clients are ", self.factory.clients
+        print "clients are ", self.clients
     def dataReceived(self, data):
+        len = 4
+
+        datarec = bytearray()
         for byte in data:
-            print repr(byte)
-            if hex(int(repr(byte))) == 0x04:
-                print 'end of message'
+
+            if hex(ord(byte)) == '0x4':
+                print 'got stop byte'
+                break
+            datarec.append(ord(byte))
+        print datarec
+
+
+
+class SmartcarFactory(Factory):
+    def __init__(self):
+        print 'initing'
+        self.tokens={}
+        self.clients = []
+    def buildProtocol(self, addr):
+        return CasperProtocol(self.clients)
 
 print sys.byteorder
 Port = int(sys.argv[1])
-factory = Factory()
-factory.clients = []
-factory.protocol = casperProtocol
-reactor.listenTCP(Port,factory)
+# factory = Factory()
+# factory.clients = []
+# factory.protocol = CasperProtocol
+reactor.listenTCP(Port,SmartcarFactory())
 print 'server started on', Port
 reactor.run()
