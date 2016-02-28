59035d5e7509d888f25b1658a478e62b335e2f0f
Andreas Fransson <Ankanx@gmail.com>
2016-02-28 14:24:25 +0100

    Working >>DEMO<< version at twistedSocketServerTest.py --> takes values sent from client and sends a number to the arduino accordingly.

---
+++ src/twistedSocketServerTest.py
@@ -0,0 +1,79 @@
+from twisted.internet.protocol import Factory, Protocol
+from twisted.internet import reactor
+from twisted.internet.serialport import SerialPort
+import sys
+import serial
+
+serServ = None
+
+class USBclient(Protocol):
+    def connectionMade(self):
+        global serServ
+        serServ = self
+        print 'Arduino device: ', serServ, ' is connected.'
+
+    def cmdReceived(self, cmd):
+        serServ.transport.write(cmd)
+        print cmd, ' - sent to Arduino.'
+        pass
+
+    def dataReceived(self,data):
+        print 'USBclient.dataReceived called with:'
+        print str(data)
+
+
+class CasperProtocol(Protocol):
+    def __init__(self,clients):
+        print 'new protocol instance'
+        self.clients = clients
+
+    def connectionMade(self):
+        self.clients.append(self)
+        print 'A client has connected'
+        print "clients are ", self.clients
+    def connectionLost(self, reason):
+        self.clients.remove(self)
+        print 'A client disconnected'
+        print "clients are ", self.clients
+    def dataReceived(self, data):
+
+        myArduino = USBclient()
+        stringit = str(data)
+        derp = stringit.strip('')
+        #derp = derp.strip('\n')
+
+        print ">"+derp+"<"
+
+        if derp == 'Direction:\n':
+            myArduino.cmdReceived("2\n")
+        elif derp == 'Direction : Right\n':
+            myArduino.cmdReceived("1\n")
+        elif derp == 'Direction : Left\n':
+            myArduino.cmdReceived("2\n")
+        elif derp == 'Direction : Center\n':
+            myArduino.cmdReceived("0\n")
+        else:
+            myArduino.cmdReceived("else\n")
+            pass
+
+
+
+
+
+class SmartcarFactory(Factory):
+    def __init__(self):
+        print 'initing'
+        self.tokens={}
+        self.clients = []
+    def buildProtocol(self, addr):
+        return CasperProtocol(self.clients)
+
+print sys.byteorder
+Port = int(sys.argv[1])
+# factory = Factory()
+# factory.clients = []
+# factory.protocol = CasperProtocol
+reactor.listenTCP(Port,SmartcarFactory())
+SerialPort(USBclient(), '/dev/cu.wchusbserial410', reactor, baudrate='9600')
+print 'server started on', Port
+reactor.run()
