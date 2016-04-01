506a8e02f9d84d2c2981b23b6161b0e494201298
Linus Eiderström Swahn <es.linus@gmail.com>
2016-04-01 16:41:53 +0200

    Fixed the protocol on the vide server.

--- src/UDPVideoServer.py
+++ src/UDPVideoServer.py
@@ -23,36 +23,65 @@ class CasperProtocol(DatagramProtocol):
 
     def outputs(self, data, (host, port)):
           stream = io.BytesIO()
+
           for i in range(20):
               # This returns the stream for the camera to capture to
               yield stream
-
+              self.imageNumber += 1
               stream.seek(0)
               b = stream.read()
 
-              packetLen = 512
-              packets = int(math.ceil(len(b)/512.0))
+              packetLen = 8000
+              packets = int(math.ceil(len(b)/8000.0))
 
-              message = "V" + str(packets) + str(len(b))
+              message = bytearray()
+              message.append(0x01)
+              message.append('V')
+
+              message.append((self.imageNumber>>24) & 0xff)
+              message.append((self.imageNumber>>16) & 0xff)
+              message.append((self.imageNumber>>8) & 0xff)
+              message.append(self.imageNumber & 0xff)
+
+              message.append(packets)
+
+              length = len(b)
+              message.append((length>>24) & 0xff)
+              message.append((length>>16) & 0xff)
+              message.append((length>>8) & 0xff)
+              message.append(length & 0xff)
+
               print message
               self.transport.write(message, (host, port))
-              print "%f, %d" % (len(b)/512.0, packets)
+              print "%f, %d" % (len(b)/8000.0, packets)
 
               for i in range(0, packets):
 
+                  message = bytearray()
+                  message.append(0x02)
+
+                  message.append((self.imageNumber>>24) & 0xff)
+                  message.append((self.imageNumber>>16) & 0xff)
+                  message.append((self.imageNumber>>8) & 0xff)
+                  message.append(self.imageNumber & 0xff)
+
+                  message.append(i)
+
                   if i==packets-1:
-                      message = b[packetLen*i:]
+                      message.extend(b[packetLen*i:])
                       self.transport.write(message, (host, port))
 
                   else:
-                      message = b[packetLen*i:packetLen*(i+1)]
+                      message.extend(b[packetLen*i:packetLen*(i+1)])
                       self.transport.write(message, (host, port))
+                  #print 'packet ' + str(i) + ' sent.'
+                  time.sleep(0.001)
 
               stream.seek(0)
               stream.truncate()
 
     def datagramReceived(self, data, (host, port)):
-
+        self.imageNumber = 0
         with picamera.PiCamera() as camera:
             camera.resolution = (640, 480)
             camera.framerate = 40
@@ -66,14 +95,11 @@ class CasperProtocol(DatagramProtocol):
                 else:
 
                     start = time.time()
-                    try:
-                        camera.capture_sequence(self.outputs(data, (host, port)), 'jpeg', use_video_port=True)
-                    except Exception as e:
-                        self.connected = False
-                        print 'bajs'
-                        break
+
+                    camera.capture_sequence(self.outputs(data, (host, port)), 'jpeg', use_video_port=True)
+
                     finish = time.time()
-                    print('Captured 20 images at %.2ffps' % (20 / (finish - start)))
+                    print('Captured 1 images at %.2ffps' % (40 / (finish - start)))
 
     def connectionLost(self):
         self.connected = False
