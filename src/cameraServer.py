7b4eabd7f370bfec30561890d528588eed857db5
Axel Spångberg <axspangberg@gmail.com>
2016-06-04 13:44:39 +0200

    added cameraServer to the develop-branch
    dsa

---
+++ src/cameraServer.py
@@ -0,0 +1,188 @@
+from twisted.internet.protocol import Factory, Protocol
+from twisted.internet import reactor
+from twisted.internet.serialport import SerialPort
+
+import sys
+import serial
+import sqlite3
+import datetime
+#import leveldb
+
+from enum import Enum
+
+
+## make sure videofeed keeps a list of videofeed observers so it can update several in one
+class moveFlag(Enum):
+    Camera = 'C'
+class cameraFlag(Enum):
+    Up = 'U'
+    Down = 'D'
+    Idle = 'I'
+class cameraAngleFlag(Enum):
+    Right = 'R'
+    Left  = 'L'
+    Idle  = 'I'
+
+serServ = None
+
+class USBclient(Protocol):
+    def connectionMade(self):
+        global serServ
+        serServ = self
+        #serServ.transport.write()
+        print 'Arduino device: ', serServ, ' is connected.'
+
+    def cmdReceived(self, cmd):
+        #serServ.transport.write(cmd)
+        for byte in cmd:
+            serServ.transport.write(chr(byte))
+        #leaving all newlines for debug reasons
+        #print cmd, ' - sent to Arduino.'
+
+    def dataReceived(self,data):
+        print 'USBclient.dataReceived called with:'
+        print data
+
+class CasperProtocol(Protocol):
+
+    localToken = None
+
+    def __init__(self,clients):
+        print 'new protocol instance'
+        self.clients = clients
+        self.localToken = (None,datetime.datetime.now)
+        # get db instance
+        #self.level = leveldb.LevelDB('path')
+
+    def connectionMade(self):
+        self.clients.append(self)
+        print 'A client has connected'
+        print "clients are ", self.clients
+    def connectionLost(self, reason):
+        self.clients.remove(self)
+        print 'A client disconnected'
+        print "clients are ", self.clients
+    def dataReceived(self, data):
+        
+        myArduino = USBclient()
+        CR   =  ''
+        LF    =  ''
+
+        datarec = bytearray()
+        for byte in data:
+            datarec.append(ord(byte))
+            if hex(ord(byte)) == '0xd':
+                CR = byte
+                continue
+            if hex(ord(byte)) == '0xa':
+                LF = byte
+                continue
+            if CR != '' and LF != '':
+                if hex(ord(byte)) == '0x4' and hex(ord(CR)) == '0xd' and hex(ord(LF)) == '0xa':
+                    print 'got CL,RF,EOF' 
+                    break
+
+        # We no have a Message! A good starting point would be to verify the user token.
+
+        if datarec[0] != ord(cameraFlag.Camera):
+             print repr(datarec[0])
+             print 'unknown cameraFlag...aborting'
+             return
+        else:
+            tf = cameraFlag.Camera
+            print 'this is a moveMsg'
+
+
+        token = str(datarec[1:17])
+
+        print repr(token)
+        print repr(self.localToken)
+
+        if token !=  self.localToken[0] or  self.localToken[1] - datetime.datetime.now() < datetime.timedelta(minutes = 5):
+            print "Open DB"
+            conn = sqlite3.connect('/home/pi/CASPER/db.db', detect_types=sqlite3.PARSE_DECLTYPES)
+            c = conn.cursor()
+            c.execute("select token, expiration from tokens where token=? and expiration>?", (token, datetime.datetime.now()))
+
+            row = c.fetchone()
+            print row
+            if row is None:
+                print 'No token found.'
+                return
+            else:
+                self.localToken = row
+
+            c.execute("update tokens set expiration=? where userId=?", (datetime.datetime.now() + datetime.timedelta(minutes = 25), row[0]))
+            conn.commit()
+
+
+        if datarec[17] == ord(cameraFlag.Up) or datarec[17] == ord(cameraFlag.Down) or datarec[17] == ord(cameraFlag.Idle):
+            if datarec[17] == ord(cameraFlag.Up):
+                print 'direction: Up'
+                cfu = cameraFlag.Up
+            if datarec[17] == ord(cameraFlag.Down):
+                print 'direction: Back'
+                cfd = cameraFlag.Down
+            if datarec[17] == ord(cameraFlag.Idle):
+                print 'direction: Idle'
+                cfi = cameraFlag.Idle
+        else:
+            print 'unknown cameraFlag..aborting'
+            return
+        if datarec[18] == ord(cameraAngleFlag.Right) or datarec[18] == ord(cameraAngleFlag.Left) or datarec[18] == ord(cameraAngleFlag.Idle):
+            if datarec[18] == ord(cameraAngleFlag.Right):
+                print 'cameraAngle: Right'
+                cafr = cameraAngleFlag.Right
+            if datarec[18] == ord(cameraAngleFlag.Left):
+                print 'cameraAngle: Left'
+                cafl = cameraAngleFlag.Left
+            if datarec[18] == ord(cameraAngleFlag.Idle):
+                print 'cameraAngle: Idle'
+                cafi = cameraAngleFlag.Idle
+        else:
+            print 'unknown cameraAngleFlag..aborting'
+            for i,byte in enumerate(datarec):
+                print repr(byte),' '
+            return
+        if datarec[19] < 0 or datarec[19] > 255:
+            print 'invalid Speed Value, aborting...'
+            return
+        else:
+            speed = datarec[19]
+            print 'speed : ',speed
+        if datarec[20] < 0 or datarec[20] > 90:
+            print 'invalid Angle Value, aborting...'
+            return
+        else:
+            angle = datarec[20]
+            print 'angle : ',angle
+
+        print 'successfully parsed buffer!, sending to serial'
+
+        message = bytearray()
+        message.append(datarec[0])
+        message.extend(datarec[17:24])
+        print repr(message)
+        myArduino.cmdReceived(message)
+        #
+        # for i,byte in enumerate(datarec):
+        #     print repr(byte),' '
+
+
+class SmartcarFactory(Factory):
+    def __init__(self):
+        print 'initing'
+        self.tokens={}
+        self.clients = []
+    def buildProtocol(self, addr):
+        return CasperProtocol(self.clients)
+
+print sys.byteorder
+Port = int(sys.argv[1])
+# factory = Factory()
+# factory.clients = []
+# factory.protocol = CasperProtocol
+reactor.listenTCP(Port,SmartcarFactory())
+SerialPort(USBclient(), '/dev/cu.wchusbserial1410', reactor, baudrate='9600')
+print 'server started on', Port
+reactor.run()
