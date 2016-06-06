dbc6c6eef1b15e6187e55b95ee113763bc4dfef4
Haxxflaxx <daniel.arnqvist@gmail.com>
2016-06-06 16:33:58 +0200

    Remove stupid crap in test

--- src/TestClients/cameraControlTest.py
+++ src/TestClients/cameraControlTest.py
@@ -9,34 +9,34 @@ def sendCmd(cmd):
         ser.write(chr(byte))
 
 message = bytearray()
-message.append(hex('C'))
-message.extend(hex('I'))
-message.extend(0x0)
-message.extend(hex('I'))
-message.extend(0x0)
-message.extend(0xd)
-message.extend(0xa)
+message.append(ord('C'))
+message.append(ord('I'))
+message.append(0x0)
+message.append(ord('I'))
+message.append(0x0)
+message.append(0xd)
+message.append(0xa)
 
 i = 0
 while True:
     if i == 0:
-        message[1] = hex('L')
-        message[2] = hex(90)
+        message[1] = ord('L')
+        message[2] = 90
     elif i == 1:
-        message[1] = hex('R')
-        message[2] = hex(90)
+        message[1] = ord('R')
+        message[2] = 90
     elif i == 2:
-        message[3] = hex('U')
-        message[4] = hex(90)
+        message[3] = ord('U')
+        message[4] = 90
     elif i == 3:
-        message[3] = hex('D')
-        message[4] = hex(50)
+        message[3] = ord('D')
+        message[4] = 50
         i = 0
 
     sendCmd(message)
     time.sleep(3)
 
-    message[1] = hex('I')
-    message[2] = hex(0)
-    message[3] = hex('I')
-    message[4] = hex(0)
+    message[1] = ord('I')
+    message[2] = 0
+    message[3] = ord('I')
+    message[4] = 0
